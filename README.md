ad4f149171336f7ff18cdfc49c8301350cb1215e
root <root@raspberrypi.(none)>
2015-06-05 20:21:43 +0000

    example udp pwm

---
+++ README.md
@@ -0,0 +1,1 @@
+#raspberry-udp-pwm
---
+++ udpPwm.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+
+import socket
+import time
+import sys
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+pwm = GPIO.PWM(18, 100)
+pwm.start(5)
+
+pwm.ChangeDutyCycle(17.5)
+
+UDP_IP = "192.168.8.103"
+UDP_PORT = 5005
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+while True:
+    data, addr = sock.recvfrom(1024)
+    print "msg:", float(data) 
+    pwm.ChangeDutyCycle( float(data) )
